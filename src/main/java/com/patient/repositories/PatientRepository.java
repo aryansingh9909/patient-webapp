b416319406e7edaa4cd7d4cd20c8069dc942cb9d
saketl2 <saketlakh2@gmail.com>
2020-09-18 06:48:42 +0000

    search query updated

--- src/main/java/com/patient/repositories/PatientRepository.java
+++ src/main/java/com/patient/repositories/PatientRepository.java
@@ -9,10 +9,16 @@ import org.springframework.data.repository.CrudRepository;
 
 
 public interface PatientRepository extends CrudRepository<Patient, String>{
+    
+    //for finding exact match of  fname and lname
+   /*@Query("{'$or':[ {'fname':?0}, {'lname':?1} ] }")
+    List<Patient> getByFnameAndLname(String fname, String lname);*/
 
-   @Query("{'$or':[ {'fname':?0}, {'lname':?1} ] }")
+    //works as  %LIKE% of SQL
+    @Query(value = "{$or:[{fname:{$regex:?0,$options:'i'}},{lname:{$regex:?1,$options:'i'}}]}")
     List<Patient> getByFnameAndLname(String fname, String lname);
 
+    //query of get only by name if only name field is there
     //List<Patient> getByName(String Name); 
     
 }
